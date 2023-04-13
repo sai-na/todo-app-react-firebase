94da06a01bbdc40121c5d5ddd5feb9e32329097d
sainath378 <92171156+sai-na@users.noreply.github.com>
2023-04-13 13:52:54 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import { AiOutlinePlus } from 'react-icons/ai';
-import Todo from './Todo';
-import { db } from './firebase';
+import React, { useState, useEffect } from 'react'
+import { AiOutlinePlus } from 'react-icons/ai'
+import Todo from './Todo'
+import { db } from './firebase'
 import {
   query,
   collection,
@@ -10,65 +10,87 @@ import {
   doc,
   addDoc,
   deleteDoc,
-} from 'firebase/firestore';
+} from 'firebase/firestore'
 
 const style = {
-  bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`,
-  container: `bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4`,
-  heading: `text-3xl font-bold text-center text-gray-800 p-2`,
-  form: `flex justify-between`,
-  input: `border p-2 w-full text-xl`,
-  button: `border p-4 ml-2 bg-purple-500 text-slate-100`,
-  count: `text-center p-2`,
-};
+  bg: `min-h-screen w-screen p-4 bg-gradient-to-r from-rose-400 to-orange-300`,
+  container: `bg-white max-w-[800px] w-full mx-auto rounded-md shadow-lg p-4`,
+  heading: `text-4xl font-bold text-center text-${'#4285F4'} p-2`,
+  form: `flex justify-between items-center mb-4`,
+  input: `border-2 border-gray-300 p-2 w-full text-xl focus:outline-none focus:border-${'#4285F4'} rounded`,
+  button: `border-2 border-transparent px-4 py-2 bg-gradient-to-r from-orange-400 to-rose-400 text-white transition-colors duration-200`,
+  count: `text-center p-2 text-gray-600`,
+}
+
+
 
 function App() {
-  const [todos, setTodos] = useState([]);
-  const [input, setInput] = useState('');
+  const [todos, setTodos] = useState([])
+  const [input, setInput] = useState('')
 
   // Create todo
   const createTodo = async (e) => {
-    e.preventDefault(e);
+    e.preventDefault(e)
     if (input === '') {
-      alert('Please enter a valid todo');
-      return;
+      alert('Please enter a valid todo')
+      return
     }
     await addDoc(collection(db, 'todos'), {
       text: input,
       completed: false,
-    });
-    setInput('');
-  };
+    })
+    setInput('')
+  }
 
   // Read todo from firebase
   useEffect(() => {
-    const q = query(collection(db, 'todos'));
+    const q = query(collection(db, 'todos'))
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
-      let todosArr = [];
+      let todosArr = []
       querySnapshot.forEach((doc) => {
-        todosArr.push({ ...doc.data(), id: doc.id });
-      });
-      setTodos(todosArr);
-    });
-    return () => unsubscribe();
-  }, []);
+        todosArr.push({ ...doc.data(), id: doc.id })
+      })
+      console.log(todosArr)
+      setTodos(todosArr)
+    })
+    return () => unsubscribe()
+  }, [])
+
+
+
+
+
+
+
 
   // Update todo in firebase
   const toggleComplete = async (todo) => {
-    await updateDoc(doc(db, 'todos', todo.id), {
-      completed: !todo.completed,
-    });
-  };
+    try {
+      await updateDoc(doc(db, 'todos', todo.id), {
+        completed: !todo.completed,
+      })
+    } catch (error) {
+      console.error('Error updating todo:', error)
+    }
+  }
+
+
+
 
   // Delete todo
   const deleteTodo = async (id) => {
-    await deleteDoc(doc(db, 'todos', id));
-  };
+    try {
+      await deleteDoc(doc(db, 'todos', id))
+    } catch (error) {
+      console.error('Error deleting todo:', error)
+    }
+  }
+
 
   return (
     <div className={style.bg}>
       <div className={style.container}>
-        <h3 className={style.heading}>Todo App</h3>
+        <h3 className={style.heading}>Task list</h3>
         <form onSubmit={createTodo} className={style.form}>
           <input
             value={input}
@@ -96,7 +118,7 @@ function App() {
         )}
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,31 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore"
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: YOUR_API_KEY,
+//   authDomain: YOUR_AUTHDOMAIN,
+//   projectId: YOUR_PROJECTID,
+//   storageBucket: YOUR_STORAGEBUCKET,
+//   messagingSenderId: YOUR_MESSAGING_SENDER_ID,
+//   appId: YOUR_APPID
+// };
+
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: YOUR_API_KEY,
-  authDomain: YOUR_AUTHDOMAIN,
-  projectId: YOUR_PROJECTID,
-  storageBucket: YOUR_STORAGEBUCKET,
-  messagingSenderId: YOUR_MESSAGING_SENDER_ID,
-  appId: YOUR_APPID
-};
+  apiKey: "AIzaSyAf3Cl47EZWnrwbcFnv1qnl4HtMxrB0lk8",
+  authDomain: "todo-test-b9ae4.firebaseapp.com",
+  projectId: "todo-test-b9ae4",
+  storageBucket: "todo-test-b9ae4.appspot.com",
+  messagingSenderId: "550119004993",
+  appId: "1:550119004993:web:c964d445445062ea04dfc0"
+}
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig)
 export const db = getFirestore(app)
